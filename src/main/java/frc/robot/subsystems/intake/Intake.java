65a6796007152532edcbd893a0d2dd772e395455
Orange Robotics <orangerobotics@gmail.com>
2026-02-02 16:28:28 -0500

    Intake command skeleton
    idk if i did this right

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,21 +1,42 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class Intake {
-   IntakeIOInputsAutoLogged inputs;
-   IntakeIO io;
+public class Intake extends SubsystemBase {
+    IntakeIOInputsAutoLogged inputs;
+    IntakeIO io;
 
-
-
-
-   public Intake(IntakeIO io) {
-       this.io = io;
-       inputs = new IntakeIOInputsAutoLogged();
+    public Intake(IntakeIO io) {
+        this.io = io;
+        inputs = new IntakeIOInputsAutoLogged();
       
-   }
+    }
 
+    public void intakeDown() {
+        io.setTargetIntakePositionDegrees(0); 
+    }
 
+    public void intakeUp() {
+        io.setTargetIntakePositionDegrees(30);
+    }
 
+    public void suck() {
+        io.setRollerTopVolts(5);
+        io.setRollerBottomVolts(5);
+    }
+
+    public Command intakeDownCommand() {
+        return this.runOnce(() -> intakeDown());
+    }
+
+    public Command intakeUpCommand() {
+        return this.runOnce(() -> intakeUp());
+    }
+
+    public Command suckCommand() {
+        return this.run(() -> suck());
+    }
 
 }
 
