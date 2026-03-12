5b98630ccbf8a941a2f217cfca6cbc6394f671f0
Orange Robotics <orangerobotics@gmail.com>
2026-03-12 15:24:40 -0400

    push before practice 3

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -31,8 +31,8 @@ public class Intake extends SubsystemBase {
     }
 
     public void intakeRunRollers() {
-        io.setTargetRollerBottomVelocity(40.0);
-        io.setTargetRollerTopVelocity(40.0);
+        io.setTargetRollerBottomVelocity(45.0);
+        io.setTargetRollerTopVelocity(45.0);
     }
 
     public void intakeRollersStop() {
@@ -41,8 +41,8 @@ public class Intake extends SubsystemBase {
     }
 
     public void reverseIntake() {
-        io.setTargetRollerBottomVelocity(-40.0);
-        io.setTargetRollerTopVelocity(-40.0);
+        io.setTargetRollerBottomVelocity(-45.0);
+        io.setTargetRollerTopVelocity(-45.0);
     }
 
     public void intakeDown() {
